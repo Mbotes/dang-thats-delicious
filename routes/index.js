bad074f71a2013a9330f00afc4cb0553ffd56360
michaelOH <michael@ourhood.co.za>
2017-05-09 22:50:26 +0200

    -added hello test page for pug -tested router to get Response and Request data

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const mike = { name: 'Michael', age: 100, cool: false};
+  // res.send('Works!');
+    res.render('hello', {
+      name: 'Mike',
+      dog: req.query.dog
+    });
+});
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
 });
 
 module.exports = router;
