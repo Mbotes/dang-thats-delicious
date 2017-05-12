ff430039b4e4b7f0f7f0cddb2141026176ec06e0
michaelOH <michael@ourhood.co.za>
2017-05-12 21:24:02 +0200

    - added editStore and updateStore controller in storeController.js - added POST /add/:id route and GET /stores/:id/edit routes to index.js  -edited both storeCard and storeForm mixins

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -19,3 +19,25 @@ exports.getStores = async (req, res) => {
     const stores = await Store.find();
     res.render('stores', {title: 'Stores', stores});
 };
+
+exports.editStore = async (req, res) => {
+    //find the store given the ID
+    const store = await Store.findOne({ _id: req.params.id});
+
+    //2. confirm a user owns the store they want to edit
+    //TODO
+
+    //3. Render out the edit form for the user to edit their store
+    res.render('editStore', { title: `Edit ${store.name}`, store});
+};
+
+exports.updateStore = async (req, res) => {
+    //find and update the store
+    const store = await Store.findOneAndUpdate({_id: req.params.id}, req.body, {
+        new: true, //return new store and not the old one
+        runValidators: true
+    }).exec();
+    req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}"> View Store </a>`);
+    res.redirect(`/stores/${store.id}/edit`);
+    //redirect them to store and tell them update worked
+};
