0de7c4eed2ad6d9a64b45629c28f2ae3b484e6f1
michaelOH <michael@ourhood.co.za>
2017-06-10 14:46:29 +0200

    -added favoriting stores to user and favorites page -added reviews form and submission

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 const multer = require('multer');
 const jimp = require('jimp');
+const User = mongoose.model('User');
 const uuid = require('uuid');
 const multerOptions = {
     storage: multer.memoryStorage(),
@@ -147,4 +148,24 @@ exports.mapStores = async (req, res) => {
 
 exports.mapPage = (req,res) => {
     res.render('map', { title: 'Map' });
+};
+
+exports.heartStore = async (req, res) => {
+    const hearts = req.user.hearts.map(obj => obj.toString());
+    const operator = hearts.includes(req.params.id) ? '$pull' : '$addToSet';
+    const user = await User
+        .findByIdAndUpdate(req.user._id,
+        { [operator]: { hearts: req.params.id }},
+        { new: true }
+    );
+
+    res.json(user);
+};
+
+exports.getHearts = async (req, res) => {
+  const stores = await Store.find({
+      _id: { $in: req.user.hearts }
+  });
+
+  res.render('stores', {title: 'Hearted Stores', stores });
 };
