1af664c5c4d78c49fde5b6ef49a07ef79565a925
michaelOH <michael@ourhood.co.za>
2017-06-11 01:09:02 +0200

    -added pagination -added reviews capabilities -added top restaurant based on review ranking

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ const { catchErrors } = require('../handlers/errorHandlers');
 // Do work here
 router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
+router.get('/stores/page/:page', catchErrors(storeController.getStores));
 router.get('/add',
     authController.isLoggedIn,
     storeController.addStore);
@@ -62,6 +63,8 @@ router.get('/hearts', authController.isLoggedIn,catchErrors(storeController.getH
 router.post('/reviews/:id',
     authController.isLoggedIn,
     catchErrors(reviewController.addReview));
+
+router.get('/top', catchErrors(storeController.getTopStores));
 /*
     API
  */
